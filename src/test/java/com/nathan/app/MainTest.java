e17b9d0136f243bf00c2ee13d0790a66fb4c1673
n8-n <n8nflynn@gmail.com>
2017-06-09 20:27:48 +0100

    Rest API Example

---
+++ src/test/java/com/nathan/app/MainTest.java
@@ -0,0 +1,12 @@
+package com.nathan.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+
+public class MainTest extends TestCase {
+    
+    public void testApp() {
+        assertTrue( true );
+    }
+}
